956432889b896c049bdc954cc1b8449c3b89b15e
Rohan Kale <rohankale67@gmail.com>
2025-02-22 19:36:40 +0000

    todo list with delete and add

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -15,13 +15,28 @@ console.log('todos',initialTodos)
 function AddTodo () {
   const [todos, setTodos] = useState(initialTodos);
   const [title, setTitle] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  //id of todos
+const newId = todos.length > 0 ? Math.max(...todos.map(t => t.id)) + 1 : 0;
 
+  function deleteTodo(id) {
+    console.log('delete me', id);
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  function editTodo(id) {
+
+  }
   return (
     <>
      <ul>
-        {todos.map(item => (
-          <Task key = {item.id} todo={item.title}/>
-          
+        {todos.map((item => (
+          <Task key = {item.id} todo={item.title} 
+          deleteButton={() => deleteTodo(item.id)}
+          editButton={() => editTodo(item.id)}
+          />)
+         
         ))}
       </ul>
      <input
@@ -30,20 +45,22 @@ function AddTodo () {
         onChange={e => setTitle(e.target.value)}
       />
        <button onClick={() => {
+
         setTitle('');
         setTodos([...todos,{
-          id: todos.length,
+          id: newId,
           title:title
         }]);
+        
       }}>Add</button>
        
     </>
   )
 }
 
-function Task ( { todo }) {
+function Task ( { todo , deleteButton, editButton }) {
   const [todoCheck, setTodoCheck] = useState(false)
-//todo = 'task1'
+  
 
 let crossDeciderLocal
 let crossLocal = "line-through"
@@ -53,17 +70,23 @@ todoCheck ? crossDeciderLocal=crossLocal: crossDeciderLocal=noCrossLocal
 function handleChangeLocal(e) { 
 setTodoCheck(e.target.checked)
 }
+
+
   return (
+    <>
+   
   <label>
         <input
         type="checkbox"
-       
         onChange={handleChangeLocal}
       />
-     
         <span style={{textDecoration: crossDeciderLocal}}>{todo}</span>
-        
         </label> 
+      <div id="buttonsAddEdit">
+        <button onClick={deleteButton}>Delete</button>
+        <button onClick={editButton}>Edit</button>
+        </div>
+        </>
   )
 }
 
@@ -93,11 +116,3 @@ function App() {
 export default App
 
 
-{
-  /*
-  #Problem: it checks everything at once 
-  #checking individual boxes 
-  # Make a function to cross text 
-  # Create a button to add items 
-  */
-}
