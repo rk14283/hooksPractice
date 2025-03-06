f42b401f2d56fe897f7aa758588d21844024aa7b
Rohan Kale <rohankale67@gmail.com>
2025-03-06 17:28:15 +0000

    weather app using useEffect hook

---
+++ use-effect-hooks-practice/react-weather/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+
+
+  const API_KEY ="57b1fe20064b916ba73ab0d32a949dc1";
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("paris");
+
+  useEffect(()=>{
+    const fetchWeatherData = async (cityName) => {
+      try {
+      const url = `https://api.weatherstack.com/current?access_key=eda493856e8c9a32da3b80b811f244c5&query=${cityName}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchWeatherData(city)
+},[city]);
+console.log(weatherData)
+//info we need: cityName, temprature, condition(clouds), humidity, wind speed
+
+if (weatherData == null)
+{
+  console.log('waiting for data')
+}
+else{
+  console.log('cityName',weatherData.location.name)
+  console.log('temprature',weatherData.current.
+    temperature)
+console.log('condition(clouds)',weatherData.current.weather_descriptions[0])
+console.log('humidity',weatherData.current.humidity)
+console.log('wind speed',weatherData.current.wind_speed)
+}
+
+{/*
+console.log('cityName',weatherData.location.name)
+console.log('temprature',weatherData.current.temprature)
+console.log('condition(clouds)',weatherData.current.weather_descriptions[0])
+console.log('humidity',weatherData.current.humidity)
+console.log('wind speed',weatherData.current.windspeed)
+*/
+}
+
+let weatherFragment = <div></div>
+if (weatherData == null)
+  {
+    console.log('waiting for data')
+  }
+  else {
+   weatherFragment = 
+    <>
+        <div className="header">
+          <h1 className="city">{weatherData.location.name}</h1>
+          <p className="temperature"> {weatherData.current.temperature}°F</p>
+          <p className="condition">{weatherData.current.weather_descriptions[0]}</p>
+        </div>
+        <div className="weather-details">
+          <div >
+            <p >Humidity</p>
+            <p style={{fontWeight:"bold"}}>{weatherData.current.humidity}%</p>
+          </div>
+          <div>
+            <p>Wind Speed</p>
+            <p style={{fontWeight:"bold", marginRight:50}}>{weatherData.current.wind_speed} mph</p>
+          </div>
+        </div>
+      </>
+  }
+  return(
+   weatherFragment
+    )}
+  
+
+export default App
